96253f2d23834997d56ae6c487c6e8bf1d271357
ishan2491 <ishanpatel2001@gmail.com>
2025-04-07 07:15:56 +0000

    feat(ec2): add security group in main.tf allowing ssh and http traffic

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -37,3 +37,47 @@ data "aws_subnets" "private_subnets" {
   }
 }
 
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# Security Group for Web Servers
+resource "aws_security_group" "web_sg" {
+  name        = "${var.vpc_prod}-WebServer-SG"
+  description = "Allow HTTP and SSH inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.vpc_prod}-WebServer-SG"
+    Environment = var.prod
+  }
+}
+
+

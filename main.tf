00d38431e597a9cf6be1c8945b53781cc6edfa6c
ishan2491 <ishanpatel2001@gmail.com>
2025-04-07 16:28:37 +0000

    feat(root): add AWS provider, remote VPC state retrieval, EC2 module call with target group, and root outputs

--- main.tf
+++ main.tf
@@ -7,3 +7,31 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Retrieve VPC outputs from the remote state of the previously applied VPC module
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "final-project-acs730-ishan"  # Same S3 bucket where the VPC state is stored
+    key    = "vpc/terraform.tfstate"         # Path to the VPC state file
+    region = "us-east-1"
+  }
+}
+
+# Call EC2 Module using the VPC details obtained from remote state
+module "ec2" {
+  source = "./modules/ec2"
+
+  vpc_id          = data.terraform_remote_state.vpc.outputs.vpc_id
+  public_subnets  = data.terraform_remote_state.vpc.outputs.public_subnets
+  private_subnets = data.terraform_remote_state.vpc.outputs.private_subnets
+
+  instance_type    = "t2.micro"
+  min_size         = 1
+  max_size         = 4
+  desired_capacity = 2
+  target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:123456789012:targetgroup/my-target-group/1234567890abcdef"
+}
---
+++ output.tf
@@ -0,0 +1,34 @@
+output "web_server_1_public_ip" {
+  description = "Public IP of Web Server 1"
+  value       = module.ec2.web_server_1_public_ip
+}
+
+output "web_server_3_public_ip" {
+  description = "Public IP of Web Server 3"
+  value       = module.ec2.web_server_3_public_ip
+}
+
+output "web_server_4_public_ip" {
+  description = "Public IP of Web Server 4"
+  value       = module.ec2.web_server_4_public_ip
+}
+
+output "database_server_5_private_ip" {
+  description = "Private IP of Database Server 5"
+  value       = module.ec2.database_server_5_private_ip
+}
+
+output "database_server_6_private_ip" {
+  description = "Private IP of Database Server 6"
+  value       = module.ec2.database_server_6_private_ip
+}
+
+output "web_asg_name" {
+  description = "Name of the Auto Scaling Group"
+  value       = module.ec2.web_asg_name
+}
+
+output "bastion_host_id" {
+  description = "ID of the Bastion Host"
+  value       = module.ec2.bastion_host_id
+}
